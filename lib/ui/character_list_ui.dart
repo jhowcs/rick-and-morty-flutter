2be431aef56c408aae6a5d6283d14fbaccd19395
jonathan <jhowcs@hotmail.com>
2020-08-02 10:57:38 +0100

    Add bottomNavigation and remove character list from main

---
+++ lib/ui/character_list_ui.dart
@@ -0,0 +1,166 @@
+import 'package:flutter/material.dart';
+import 'package:rick_and_morty/bloc/character_bloc.dart';
+import 'package:rick_and_morty/model/character.dart';
+import 'package:rick_and_morty/network/character_repository.dart';
+
+import 'custom_loader.dart';
+import 'detail.dart';
+
+class CharacterListUI extends StatefulWidget {
+  @override
+  createState() => _CharacterListUIState();
+}
+
+class _CharacterListUIState extends State<CharacterListUI> {
+  ScrollController _scrollController;
+  TextEditingController _textEditingController;
+  final _bloc = CharacterBloc(CharacterRepository(), 400);
+
+  @override
+  void initState() {
+    super.initState();
+    _bloc.fetchCharacters();
+    _textEditingController = TextEditingController();
+    _scrollController = ScrollController()..addListener(_scrollListener);
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    _scrollController.dispose();
+    _textEditingController.dispose();
+    _bloc.dispose();
+  }
+
+  void _scrollListener() {
+    if (shouldLoadMore() &&
+        (_scrollController.position.pixels ==
+            _scrollController.position.maxScrollExtent)) {
+      _bloc.fetchCharacters();
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder(
+        stream: _bloc.characters,
+        builder: (context, AsyncSnapshot<List<Character>> snapshot) {
+          if (snapshot.connectionState == ConnectionState.active ||
+              snapshot.connectionState == ConnectionState.done) {
+            if (snapshot.hasData) {
+              return Column(children: <Widget>[
+                buildSearchField(),
+                Expanded(child: buildCharacterList(snapshot.data)),
+              ]);
+            } else {
+              return retryWidget();
+            }
+          }
+          return Center(child: CircularProgressIndicator());
+        });
+  }
+
+  TextFormField buildSearchField() {
+    return TextFormField(
+      controller: _textEditingController,
+      decoration: InputDecoration(
+          labelText: 'Search for characters...',
+          contentPadding: EdgeInsets.all(16)),
+      onChanged: (query) {
+        _bloc.searchOnListAlreadyFetched(query);
+      },
+    );
+  }
+
+  Widget buildCharacterList(List<Character> characterList) {
+    return ListView.separated(
+        controller: _scrollController,
+        separatorBuilder: (BuildContext _context, _index) => Divider(),
+        itemCount: characterList.length <= 0 ? 0 : characterList.length + 1,
+        itemBuilder: (BuildContext _context, int position) {
+          return position >= characterList.length
+              ? buildProgressOrReachEnd()
+              : InkWell(
+                  onTap: () {
+                    Navigator.push(
+                        _context,
+                        MaterialPageRoute(
+                            builder: (_context) => DetailScreen(
+                                  character: characterList[position],
+                                )));
+                  },
+                  child: _buildRow(characterList, position),
+                );
+        });
+  }
+
+  Widget _buildRow(List<Character> characterList, int position) {
+    return Container(
+        margin: EdgeInsets.only(left: 16, top: 4, right: 16, bottom: 4),
+        child: Row(
+          children: <Widget>[
+            ClipOval(
+              child: Image.network(
+                characterList[position].image,
+                height: 60,
+                width: 60,
+              ),
+            ),
+            Expanded(
+                child: Container(
+                    margin: EdgeInsets.only(left: 8, right: 8),
+                    child: Text(
+                      characterList[position].name,
+                      overflow: TextOverflow.ellipsis,
+                      maxLines: 1,
+                    ))),
+          ],
+        ));
+  }
+
+  Widget _loadMoreProgress() {
+    return Container(
+      margin: EdgeInsets.only(bottom: 12),
+      child: CustomLoader(width: 45.0, height: 45.0),
+    );
+  }
+
+  Widget buildProgressOrReachEnd() {
+    if (shouldLoadMore()) {
+      return _loadMoreProgress();
+    }
+  }
+
+  Widget retryWidget() {
+    return Center(
+        child: Container(
+      margin: EdgeInsets.only(left: 16, right: 16),
+      child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              'No Connection',
+              style: TextStyle(fontSize: 32, fontWeight: FontWeight.bold),
+            ),
+            Container(
+              margin: EdgeInsets.only(top: 8),
+              child: Text(
+                  'You are current offline. Please check your internet settings.',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(fontSize: 18)),
+            ),
+            Container(
+              margin: EdgeInsets.only(top: 24),
+              child: RaisedButton(
+                  color: Colors.green,
+                  textColor: Colors.white,
+                  child: Text('Retry'),
+                  onPressed: () => _bloc.fetchCharacters()),
+            ),
+          ]),
+    ));
+  }
+
+  bool shouldLoadMore() =>
+      !_bloc.hasReachedEnd() && _textEditingController.text.isEmpty;
+}
--- lib/ui/main.dart
+++ lib/ui/main.dart
@@ -1,10 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:rick_and_morty/model/character.dart';
-import 'package:rick_and_morty/network/character_repository.dart';
-import 'package:rick_and_morty/ui/custom_loader.dart';
-import 'package:rick_and_morty/ui/detail.dart';
+import 'package:rick_and_morty/ui/episodes_list_ui.dart';
+import 'package:rick_and_morty/ui/location_list_ui.dart';
 
-import '../bloc/character_bloc.dart';
+import 'character_list_ui.dart';
 
 void main() {
   runApp(App());
@@ -26,164 +24,54 @@ class App extends StatelessWidget {
 
 class Home extends StatefulWidget {
   @override
-  createState() => _HomeState();
+  _HomeState createState() => _HomeState();
 }
 
 class _HomeState extends State<Home> {
-  ScrollController _scrollController;
-  TextEditingController _textEditingController;
-  final _bloc = CharacterBloc(CharacterRepository(), 400);
-
-  @override
-  void initState() {
-    super.initState();
-    _bloc.fetchCharacters();
-    _textEditingController = TextEditingController();
-    _scrollController = ScrollController()..addListener(_scrollListener);
-  }
-
-  @override
-  void dispose() {
-    super.dispose();
-    _scrollController.dispose();
-    _textEditingController.dispose();
-    _bloc.dispose();
-  }
-
-  void _scrollListener() {
-    if (shouldLoadMore() &&
-        (_scrollController.position.pixels ==
-            _scrollController.position.maxScrollExtent)) {
-      _bloc.fetchCharacters();
-    }
-  }
+  int _currentIndex = 0;
+  final List<Widget> _children = [
+    CharacterListUI(),
+    LocationListUI(),
+    EpisodesListUI()
+  ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Rick And Morty'),
+        title: Text('Rick and Morty'),
       ),
-      body: StreamBuilder(
-          stream: _bloc.characters,
-          builder: (context, AsyncSnapshot<List<Character>> snapshot) {
-            if (snapshot.connectionState == ConnectionState.active ||
-                snapshot.connectionState == ConnectionState.done) {
-              if (snapshot.hasData) {
-                return Column(children: <Widget>[
-                  buildSearchField(),
-                  Expanded(child: buildCharacterList(snapshot.data)),
-                ]);
-              } else {
-                return retryWidget();
-              }
-            }
-            return Center(child: CircularProgressIndicator());
-          }),
+      body: _children[_currentIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        onTap: _onTabTapped,
+        currentIndex: _currentIndex,
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(
+              Icons.supervised_user_circle,
+            ),
+            title: Text('Characters'),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(
+              Icons.place,
+            ),
+            title: Text('Location'),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(
+              Icons.tv,
+            ),
+            title: Text('Episodes'),
+          ),
+        ],
+      ),
     );
   }
 
-  TextFormField buildSearchField() {
-    return TextFormField(
-      controller: _textEditingController,
-      decoration: InputDecoration(
-          labelText: 'Search for characters...',
-          contentPadding: EdgeInsets.all(16)),
-      onChanged: (query) {
-        _bloc.searchOnListAlreadyFetched(query);
-      },
-    );
+  void _onTabTapped(int index) {
+    setState(() {
+      _currentIndex = index;
+    });
   }
-
-  Widget buildCharacterList(List<Character> characterList) {
-    return ListView.separated(
-        controller: _scrollController,
-        separatorBuilder: (BuildContext _context, _index) => Divider(),
-        itemCount: characterList.length <= 0 ? 0 : characterList.length + 1,
-        itemBuilder: (BuildContext _context, int position) {
-          return position >= characterList.length
-              ? buildProgressOrReachEnd()
-              : InkWell(
-                  onTap: () {
-                    Navigator.push(
-                        _context,
-                        MaterialPageRoute(
-                            builder: (_context) => DetailScreen(
-                                  character: characterList[position],
-                                )));
-                  },
-                  child: _buildRow(characterList, position),
-                );
-        });
-  }
-
-  Widget _buildRow(List<Character> characterList, int position) {
-    return Container(
-        margin: EdgeInsets.only(left: 16, top: 4, right: 16, bottom: 4),
-        child: Row(
-          children: <Widget>[
-            ClipOval(
-              child: Image.network(
-                characterList[position].image,
-                height: 60,
-                width: 60,
-              ),
-            ),
-            Expanded(
-                child: Container(
-                    margin: EdgeInsets.only(left: 8, right: 8),
-                    child: Text(
-                      characterList[position].name,
-                      overflow: TextOverflow.ellipsis,
-                      maxLines: 1,
-                    ))),
-          ],
-        ));
-  }
-
-  Widget _loadMoreProgress() {
-    return Container(
-      margin: EdgeInsets.only(bottom: 12),
-      child: CustomLoader(width: 45.0, height: 45.0),
-    );
-  }
-
-  Widget buildProgressOrReachEnd() {
-    if (shouldLoadMore()) {
-      return _loadMoreProgress();
-    }
-  }
-
-  Widget retryWidget() {
-    return Center(
-        child: Container(
-      margin: EdgeInsets.only(left: 16, right: 16),
-      child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'No Connection',
-              style: TextStyle(fontSize: 32, fontWeight: FontWeight.bold),
-            ),
-            Container(
-              margin: EdgeInsets.only(top: 8),
-              child: Text(
-                  'You are current offline. Please check your internet settings.',
-                  textAlign: TextAlign.center,
-                  style: TextStyle(fontSize: 18)),
-            ),
-            Container(
-              margin: EdgeInsets.only(top: 24),
-              child: RaisedButton(
-                  color: Colors.green,
-                  textColor: Colors.white,
-                  child: Text('Retry'),
-                  onPressed: () => _bloc.fetchCharacters()),
-            ),
-          ]),
-    ));
-  }
-
-  bool shouldLoadMore() =>
-      !_bloc.hasReachedEnd() && _textEditingController.text.isEmpty;
 }
